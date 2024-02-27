27aac1308da699a44fdea5814b510f825202319b
ShantanuDhar <shantanu170604@gmail.com>
2024-02-27 17:47:38 +0530

    Adding Teacher generated quiz

--- lib/screens/teacher_homepage.dart
+++ lib/screens/teacher_homepage.dart
@@ -1,4 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:innovate_2/global/global_var.dart';
+import 'package:innovate_2/resources/database.dart';
+import 'package:innovate_2/screens/create_quiz.dart';
+import 'package:innovate_2/screens/quiz_play.dart';
+import 'package:innovate_2/widgets/widgets.dart';
 
 class TeacherHomepage extends StatefulWidget {
   const TeacherHomepage({super.key});
@@ -8,8 +14,139 @@ class TeacherHomepage extends StatefulWidget {
 }
 
 class _TeacherHomepageState extends State<TeacherHomepage> {
+ Stream? quizStream;
+
+ 
+  DatabaseService databaseService = new DatabaseService();
+
+  Widget quizList() {
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          children: [
+            StreamBuilder(
+              stream: FirebaseFirestore.instance.collection("Quiz").snapshots(),
+              builder: (context, snapshot) {
+                return snapshot.data == null
+                    ? Container()
+                    : ListView.builder(
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          return QuizTile(
+                    noOfQuestions: snapshot.data!.docs.length,
+                    imageUrl:
+                        snapshot.data!.docs[index].data()['quizImgUrl'],
+                    title:
+                        snapshot.data!.docs[index].data()['quizTitle'],
+                    description:
+                        snapshot.data!.docs[index].data()['quizDesc'],
+                    id: snapshot.data!.docs[index].data()['id'],
+                  );
+                        });
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    databaseService.getQuizData().then((value) {
+      quizStream = value;
+      setState(() {});
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: AppLogo(),
+        
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        //brightness: Brightness.li,
+      ),
+      body: quizList(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => CreateQuiz()));
+        },
+      ),
+    );
+  }
+}
+
+class QuizTile extends StatelessWidget {
+  final String imageUrl, title,
+   id, 
+   description;
+  final int noOfQuestions;
+
+  QuizTile(
+      {required this.title,
+      required this.imageUrl,
+      required this.description,
+      required this.id,
+      required this.noOfQuestions});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) => QuizPlay(id)
+        ));
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        height: 150,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Stack(
+            children: [
+              Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+                width: MediaQuery.of(context).size.width,
+              ),
+              Container(
+                color: Colors.black26,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        title,
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(height: 4,),
+                      Text(
+                        description,
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
